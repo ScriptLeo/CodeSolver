269ae448bdea9e47c08e97a6c4c6e8e231fb3eb2
ScriptLeo <ebmidtun@gmail.com>
2019-04-18 18:29:51 +0200

    -Implemented admin login

--- code_solver.py
+++ code_solver.py
@@ -10,7 +10,10 @@ import logging
 import traceback
 from urllib import request, error
 from tkinter import Tk, Frame, Label, Entry, TOP, RIGHT, BOTTOM, LEFT, YES, NO, NW, E, W, S, X, BOTH, END, \
-    Button, Canvas, Text, DISABLED, NORMAL, ALL, Scale, Y, N, SUNKEN, Radiobutton, IntVar, Checkbutton, BooleanVar
+    Button, Canvas, Text, DISABLED, NORMAL, ALL, Scale, Y, N, SUNKEN, Radiobutton, IntVar, Checkbutton, BooleanVar, \
+    messagebox
+import hashlib
+import uuid
 from ctypes import windll
 import configparser
 
@@ -21,24 +24,35 @@ class CodeSolver:
     # Program config
     config = {
         'system parameters': {
-            'set_dpi_awareness': 'True',
+            'set_dpi_awareness': True,
             'tesseract_directory': r'C:\Program Files\Tesseract-OCR\tesseract.exe'
         },
         'window settings': {
-            'transparent_on_lost_focus': 'True',
-            'default_transparency_alpha': '0.4',
-            'set_topmost': 'True',
-            'window_width': '700',
-            'window_height': '550',
-            'center_image_on_canvas': 'True'
+            'transparent_on_lost_focus': True,
+            'default_transparency_alpha': 0.4,
+            'set_topmost': True,
+            'window_width': 700,
+            'window_height': 550
+        },
+        'canvas settings': {
+            'resize_threshold': 1,  # Prevents resizing on small canvas changes},
+            'center_image_on_canvas': True
         }
     }
     sys_cfg = config['system parameters']  # Short names
     win_cfg = config['window settings']
+    can_cfg = config['canvas settings']
     config_parser = None
 
+    # Admin settings will always load default unless accepted admin login
+    adm_cfg = {
+        'use_local_image': False
+    }
+    # Hashed admin password
+    admin_password = '0368a552db2565de91a7c69b0ed21e3c20e6052856695566d346a68ba30c8de2:7d33d1e23a954159b5ec58a3ecd7b641'
+    admin_privileges = False
+
     # Image params/vars
-    use_local_image = False
     new_image = False
     image = None
     tk_image = None
@@ -53,7 +67,6 @@ class CodeSolver:
     image_x_offset = None
     image_y_offset = None
     bounding_boxes = None
-    resize_threshold = 1  # Prevents resizing on small canvas changes
     last_resize = None
     pending_boxes = False
 
@@ -89,7 +102,7 @@ class CodeSolver:
         self.read_config()
 
         # Necessary to get PIL to work correctly on high DPI scaling
-        if self.sys_cfg['set_dpi_awareness'] == 'True':
+        if self.sys_cfg['set_dpi_awareness']:
             user32 = windll.user32
             user32.SetProcessDPIAware()
 
@@ -110,18 +123,47 @@ class CodeSolver:
             # Read from file
             self.config_parser.read('config.ini')
 
+            # Check for admin privileges (Hidden from default config)
+            if 'ADMIN' in self.config_parser:
+                self.admin_login()
+                if self.admin_privileges:
+                    # Read requested admin settings
+                    for sub_key in self.adm_cfg:
+                        val = self.resolve_type(self.config_parser, 'ADMIN', sub_key)
+                        self.adm_cfg[sub_key] = val
+                        print('set admin config [{}] to {}'.format(sub_key, val))
+
             # Read all parameters specified in config
             print('Reading config')
             for key, sub_dict in self.config.items():
                 try:
                     for sub_key in sub_dict:
-                        val = self.config_parser[key][sub_key]
+                        val = self.resolve_type(self.config_parser, key, sub_key)
                         self.config[key][sub_key] = val
                         print('set config[{}][{}] to {}'.format(key, sub_key, val))
                 except KeyError:
                     pass
         except:
             traceback.print_exc()
+
+    @staticmethod
+    def resolve_type(parser, key, sub_key):
+        val = parser[key][sub_key]
+
+        # Boolean condition
+        if val.lower() in ('true', 'false', '1', '0', 'yes', 'no'):
+            return parser.getboolean(key, sub_key)
+
+        # Integer condition
+        elif val.isdigit():
+            return parser.getint(key, sub_key)
+
+        # Float condition
+        elif val.replace('.', '').isdigit():
+            return parser.getfloat(key, sub_key)
+
+        # Default to string
+        return parser.get(key, sub_key)
 
     def write_config(self):
         # List all parameters that should be written
@@ -138,24 +180,75 @@ class CodeSolver:
             traceback.print_exc()
 
     def window_close(self):
-        self.write_config()
-        self.root.destroy()
+        if messagebox.askyesno("Closing", "Exit program?"):
+            self.write_config()
+            self.root.destroy()
+
+    def admin_login(self):
+        admin_window = Tk()
+        admin_window.title('Admin login')
+        admin_window.geometry('200x60')
+        admin_window.attributes('-toolwindow', 1, '-topmost', True)
+        admin_window.resizable(False, False)
+        admin_window.bind('<Escape>', lambda e: admin_window.destroy())
+
+        self.center_window(admin_window)
+
+        frame_input = Frame(admin_window)
+        Label(frame_input, text='password: ').pack(side=LEFT)
+        entry_password = Entry(frame_input, show="*")
+        entry_password.focus_set()
+        entry_password.pack(side=RIGHT)
+
+        frame_buttons = Frame(admin_window)
+        Button(frame_buttons, text='cancel', width=8, command=lambda: admin_window.destroy()).pack(side=RIGHT)
+        Button(frame_buttons, text='log in', width=8,
+               command=lambda: self.verify_admin(admin_window, entry_password)).pack(side=RIGHT)
+        admin_window.bind('<Return>', lambda e: self.verify_admin(admin_window, entry_password))
+
+        frame_input.pack(side=TOP, padx=5, pady=(5, 0))
+        frame_buttons.pack(side=BOTTOM, fill=X, padx=5, pady=5)
+        admin_window.mainloop()
+
+    @staticmethod
+    def hash_password(password):
+        # uuid is used to generate a random number
+        salt = uuid.uuid4().hex
+        return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ':' + salt
+
+    @staticmethod
+    def check_password(hashed_password, user_password):
+        password, salt = hashed_password.split(':')
+        return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+
+    def verify_admin(self, parent, entry_widget):
+        password_attempt = entry_widget.get()
+        if self.check_password(self.admin_password, password_attempt):
+            print('***Accepted login as administrator***')
+            self.admin_privileges = True
+            parent.destroy()
+        else:
+            print('***Incorrect administrator password***')
+            self.timed_highlight(parent, entry_widget)
 
     def init_gui(self):
         # Root config
         self.root = Tk()
-        self.root.report_callback_exception = lambda a, b, c: self.elevate_error()
-        self.root.title('Instech Code Solver v1.0 by Eivind Brate Midtun')
+        self.root.report_callback_exception = lambda a_, b_, c_: self.elevate_error()
+        self.root.title('Instech Code Solver v1.0 by Eivind Brate Midtun'
+                        + (' (admin privileges activated)' if self.admin_privileges else ''))
         self.root.minsize(700, 550)
         self.root.geometry('{}x{}'.format(self.win_cfg['window_width'], self.win_cfg['window_height']))
-        if self.win_cfg['transparent_on_lost_focus'] == 'True':
+        if self.win_cfg['transparent_on_lost_focus']:
             self.root.bind("<FocusIn>", lambda e: self.set_root_alpha(1))
             self.root.bind("<FocusOut>", lambda e: self.set_root_alpha(
-                float(self.win_cfg['default_transparency_alpha'])))
-        if self.win_cfg['set_topmost'] == 'True':
+                self.win_cfg['default_transparency_alpha']))
+        if self.win_cfg['set_topmost']:
             self.root.wm_attributes('-topmost', True)
         self.root.protocol("WM_DELETE_WINDOW", self.window_close)
+        self.root.bind('<Escape>', lambda e: self.window_close())
         self.center_window(self.root)
+        self.root.focus_force()
 
         # Main frame
         frame_main = Frame(self.root)
@@ -166,8 +259,10 @@ class CodeSolver:
         label_url = Label(self.frame_url, text='image url:')
         self.entry_url = Entry(self.frame_url)
         self.entry_url.insert(0, "https://images.finncdn.no/dynamic/1280w/2019/4/vertical-1/10/4/144/681/364_714666085.jpg")
+        self.entry_url.focus_set()
+        self.entry_url.select_range(0, END)
         btn_grab = Button(self.frame_url, text='screen grab', width=10, command=lambda: self.image_grab())
-        btn_grab.bind("<Enter>", lambda e: self.set_root_alpha(float(self.win_cfg['default_transparency_alpha']), 1))
+        btn_grab.bind("<Enter>", lambda e: self.set_root_alpha(self.win_cfg['default_transparency_alpha'], 1))
         btn_grab.bind("<Leave>", lambda e: self.set_root_alpha(1, 2))
         btn_grab.pack(side=RIGHT, anchor=E)
         Button(self.frame_url, text='url grab', width=10,
@@ -223,7 +318,7 @@ class CodeSolver:
         self.txt_output.pack(side=BOTTOM, fill=X, expand=YES)
 
         # Pack frames
-        if self.use_local_image:
+        if self.adm_cfg['use_local_image']:
             Label(frame_main, text='Using local image', fg='red').pack(side=TOP)
         self.frame_url.pack(side=TOP, fill=X, padx=20, pady=(10, 0))
         frame_buttons.pack(side=BOTTOM, anchor=S, fill=X, expand=NO, padx=20, pady=10)
@@ -263,8 +358,8 @@ class CodeSolver:
         height = win.winfo_height()
         titlebar_height = win.winfo_rooty() - win.winfo_y()
         win_height = height + titlebar_height + frm_width
-        x = win.winfo_screenwidth() // 2 - win_width // 2
-        y = win.winfo_screenheight() // 2 - win_height // 2
+        x = max(10, win.winfo_screenwidth() // 2 - win_width // 2)
+        y = max(10, win.winfo_screenheight() // 2 - win_height // 2)
         win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
 
     def get_image(self, mode='from_url'):
@@ -278,7 +373,8 @@ class CodeSolver:
                 print('grabbed image x0:{}, y0:{}, x1:{}, y1:{}:'.format(x0, y0, x1, y1))
 
             elif mode == 'from_url':
-                image = r'resources\test_img1.jpg' if self.use_local_image else request.urlopen(self.entry_url.get())
+                image = r'resources\test_img1.jpg' if self.adm_cfg['use_local_image'] == 'True' else \
+                    request.urlopen(self.entry_url.get())
                 self.image = Image.open(image)
 
             else:
@@ -305,12 +401,17 @@ class CodeSolver:
         except FileNotFoundError:
             self.set_status('Did not find specified local image', 'red')
 
-        self.frame_url.configure(highlightcolor='red', highlightbackground='red', highlightthickness=2)
-        self.root.after(1500, self.timed_highlight)
+        self.timed_highlight(self.root, self.frame_url)
 
-    def timed_highlight(self):
+    def timed_highlight(self, parent, widget):
+        # Initiates a timed highlight
+        widget.configure(highlightcolor='red', highlightbackground='red', highlightthickness=2)
+        parent.after(1500, lambda: self.highlight_off(widget))
+
+    @staticmethod
+    def highlight_off(widget):
         # Toggles off the highlight after a set amount of time
-        self.frame_url.configure(highlightthickness=0)
+        widget.configure(highlightthickness=0)
 
     def set_status(self, text, fg):
         self.lbl_status.configure(text='Status: ' + text, fg=fg)
@@ -332,8 +433,8 @@ class CodeSolver:
 
             # Resize if canvas has changed more than threshold since last resize
             if (self.new_image
-                    or self.resize_threshold < abs(self.canvas_width - canvas_width)
-                    or self.resize_threshold < abs(self.canvas_height - canvas_height)):
+                    or self.can_cfg['resize_threshold'] < abs(self.canvas_width - canvas_width)
+                    or self.can_cfg['resize_threshold'] < abs(self.canvas_height - canvas_height)):
                 # Save new canvas size
                 self.canvas_width, self.canvas_height = canvas_width, canvas_height
 
@@ -348,7 +449,7 @@ class CodeSolver:
                 self.image_scale = self.image_width / w
 
                 # Calculate offsets
-                if self.win_cfg['center_image_on_canvas'] == 'True':
+                if self.can_cfg['center_image_on_canvas']:
                     self.image_x_offset = (self.canvas_width - self.image_width) // 2
                     self.image_y_offset = (self.canvas_height - self.image_height) // 2
                 else:
@@ -482,4 +583,4 @@ class CodeSolver:
 
 
 if __name__ == "__main__":
-    CodeSolver()
+    c = CodeSolver()
